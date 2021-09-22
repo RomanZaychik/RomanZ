323d1d13f24299824aa32ccafb4f1fc171b46cb0
RomanZaychik <romazay78@gmail.com>
2021-09-22 16:22:59 +0300

    Ninth correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	$('.header-navbar__btn').on('click', function() {
 		$(this).toggleClass('header-navbar__btn--active');
-		$('.header-navbar__list').toggleClass('header-navbar__list--active');
+		$('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
 
 });
